163cc19f24e98a69f92d0be37e11dd7ba5b36a82
Zythaar <stefangreiss@gmail.com>
2017-09-09 15:22:30 +0200

    E01 - Movement

---
+++ RPG Project/Assets/Scripts/PlayerMotor.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(NavMesh))]
+public class PlayerMotor : MonoBehaviour {
+
+    NavMeshAgent agent;
+
+	// Use this for initialization
+	void Start () {
+        agent = GetComponent<NavMeshAgent>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void MoveToPoint (Vector3 point)
+    {
+        agent.SetDestination(point);
+    }
+
+}
